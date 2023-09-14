ecb303e7e316efe2606795ef98db6514791320a3
swiftandquick <chenyMA16@gmail.com>
2023-09-13 20:21:16 -0400

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -1,7 +1,6 @@
 const BaseJoi = require('joi');
 const sanitizeHtml = require('sanitize-html');
 
-// Create an escapeHTML() method that sanitize HTML.  
 const extension = (joi) => ({
     type: 'string',
     base: joi.string(),
@@ -22,21 +21,15 @@ const extension = (joi) => ({
     }
 });
 
-// Set up an extension for BaseJoi.  
 const Joi = BaseJoi.extend(extension);
 
-// Middleware function to validate schema.  
-// Use joi to validate the schema, both title and content must be non-empty strings.  
-// Use escapeHTML() method to sanitize HTML for all text inputs.  
 module.exports.threadSchema = Joi.object({
-        thread: Joi.object({
-            title: Joi.string().required().escapeHTML(), 
-            content: Joi.string().required().escapeHTML() 
-        }).required()
-    });
+    thread: Joi.object({
+        title: Joi.string().required().escapeHTML(), 
+        content: Joi.string().required().escapeHTML() 
+    }).required()
+});
 
-// Use joi to validate the schema, replyContent must not be an empty string.  
-// Use escapeHTML() method to sanitize HTML for all text inputs.  
 module.exports.replySchema = Joi.object({
     reply: Joi.object({
         replyContent: Joi.string().required().escapeHTML()
