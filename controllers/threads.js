e233cd91cc24b6d126147e13cc37a6363dac69fa
swiftandquick <chenyMA16@gmail.com>
2023-09-13 20:13:51 -0400

    Update threads.js

--- controllers/threads.js
+++ controllers/threads.js
@@ -1,9 +1,5 @@
-// Require the models from models folder.  
 const Thread = require('../models/thread');
 
-// Populate the author object so we can display the object properties.
-// Find all campgrounds and render them in index.ejs of threads folder, pass in threads array as argument.  
-// Sort the threads array by time from latest to earliest before passing threads into index.ejs.  
 module.exports.index = async(req, res) => {
     const threads = await Thread.find({}).populate('author');
     threads.sort(function(a, b) {
@@ -12,16 +8,10 @@ module.exports.index = async(req, res) => {
     res.render('threads/index', {threads});
 }
 
-// When creating a thread, render new.ejs of threads folder.  
 module.exports.renderNewForm = (req, res) => {
     res.render('threads/new');
 }
 
-// Set the postTime, lastEditTime, and lastThreadUpdate to current time, which is the time when the thread is submitted. 
-// Set the user as the user that's currently logged in.  
-// Save the thread as the new object inside the threads table.  
-// Flash a success message if I successfully make a new thread.  
-// Redirect to the newly created thread.  
 module.exports.createThread = async(req, res) => {
     const thread = new Thread(req.body.thread);
     thread.postTime = new Date();
@@ -33,9 +23,6 @@ module.exports.createThread = async(req, res) => {
     res.redirect(`/threads/${thread._id}`);
 }
 
-// Populate the author object and the replies array of objects so we can display the object properties.  
-// If thread doesn't exist, flash an error message and redirect to the index page.  
-// If thread does exist, render a specific thread based on ID, on show.ejs of threads folder.  
 module.exports.showThread = async(req, res) => {
     const thread = await Thread.findById(req.params.id).populate({
         path: 'replies',
@@ -52,8 +39,6 @@ module.exports.showThread = async(req, res) => {
     }
 }
 
-// If the thread is not found, redirect back to localhost:3000/threads.  
-// Otherwise, render edit.ejs.  
 module.exports.renderEditForm = async(req, res) => {
     const thread = await Thread.findById(req.params.id);
     if(!thread) {
@@ -65,9 +50,6 @@ module.exports.renderEditForm = async(req, res) => {
     }
 }
 
-// Set the thread's lastEditTime to now.  
-// Flash a success message if I successfully edited a new thread.  
-// After the edit thread form is submitted, redirect to localhost:3000/threads/:id.  
 module.exports.updateThread = async(req, res) => {
     const { id } = req.params;
     const thread = await Thread.findByIdAndUpdate(id, { ...req.body.thread });
@@ -77,9 +59,6 @@ module.exports.updateThread = async(req, res) => {
     res.redirect(`/threads/${thread._id}`);
 }
 
-// Delete the thread from the database.  
-// Flash a success message if I successfully deleted a new thread.  
-// Redirect to localhost:3000/threads after deletion.  
 module.exports.deleteThread = async(req, res) => {
     const { id } = req.params;
     await Thread.findByIdAndDelete(id);
