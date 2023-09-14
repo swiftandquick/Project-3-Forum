c5e1ab9b446f987fb70ee6e286e21c0fba23f490
swiftandquick <chenyMA16@gmail.com>
2023-09-13 20:14:52 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,8 @@
-// Require packages.  
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-// Create a schema.  
 const Schema = mongoose.Schema;
 
-// Create a schema for users, which include email as a unique string.   
 const UserSchema = new Schema({
     email: {
         type: String, 
@@ -14,7 +11,6 @@ const UserSchema = new Schema({
     }
 });
 
-// Use passportLocalMongoose to add username and password to the schema.  
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', UserSchema);
