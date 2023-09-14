43ada8e441b788fdbb0aad4b0b44aca03e1f39b5
swiftandquick <chenyMA16@gmail.com>
2023-09-13 20:14:30 -0400

    Update reply.js

--- models/reply.js
+++ models/reply.js
@@ -1,13 +1,7 @@
-// Require packages.  
 const mongoose = require('mongoose');
 
-// Create a schema.  
 const Schema = mongoose.Schema;
 
-// Create a schema for replies, which include replyContent as string, 
-// author is the User object, which is the user that created the reply, 
-// replypostTime as date, which indicates when the reply was made, 
-// replyLastEditTime as date, which indicates when the reply was last edited. 
 const replySchema = new Schema({
     replyContent: String,
     author: {
